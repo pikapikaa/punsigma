ad49e56b8a8f643fcbf57d95fe74b97c0be91dc3
admin <gorod3012@gmail.com>
2023-10-27 01:27:29 +0800

    *fix

--- src/services/fakeData.ts
+++ src/services/fakeData.ts
@@ -193,79 +193,6 @@ export const podcasts: Podcast[] = [
   },
 ];
 
-export const allTopics: Topic[] = [
-  {
-    id: `1`,
-    url: 'https://teletype.in/files/d9/6a/d96aa6ca-5cc0-49bd-b66b-4f58f9f9a564.jpeg',
-    description: '',
-    title: 'Жилой дом. Квартира.',
-    date: '12.12.12',
-  },
-  {
-    id: `2`,
-    url: 'https://begeton.com/files/users-companies/122/3/08/nYB5El3FbbEo7CB67cW3M8qWSjltQEuI.jpg.jpeg',
-    description: '',
-    title: 'Продукты питания',
-    date: 'string',
-  },
-  {
-    id: `3`,
-    url: 'https://cdn.kanobu.ru/editor/images/14/ad828eca-6516-4c8d-bffc-7ea8e19caffa.webp',
-    description: '',
-    title: 'Эмоции',
-    date: 'string',
-  },
-  {
-    id: `4`,
-    url: 'https://primamediamts.servicecdn.ru/f/big/911/910099.jpg',
-    description: '',
-    title: 'В банке',
-    date: 'string',
-  },
-  {
-    id: `5`,
-    url: 'https://assets3.cbsnewsstatic.com/hub/i/r/2017/04/06/0f3e9754-3fd8-481a-bf80-97c1c38c0cf1/thumbnail/1200x630/c8bc2b7238dd83306cefecd875f56a3a/bodegas-20161144.jpg?v=a6a127b4a243923dd1e2140b75f2a43c',
-    description: '',
-    title: 'В магазине',
-    date: 'string',
-  },
-  {
-    id: `6`,
-    url: 'https://static.life.ru/publications/2020/5/25/1143568001582.5872.jpg',
-    description: '',
-    title: 'В аэропрту',
-    date: 'string',
-  },
-];
-
-export const recentlyPodcasts = [
-  {
-    id: 1,
-    text: 'Mr. Robot by Sam Esmail for USA Network',
-    url: `https://images-na.ssl-images-amazon.com/images/S/pv-target-images/108517acee920b44a8679bf2dd0d9ec29f6728257513e62fd6b5610529185caf._RI_TTW_.jpg`,
-  },
-  {
-    id: 2,
-    text: 'Mr. Robot. "Eps1.1_ones-and-zer0es.mpeg"',
-    url: `https://s.yimg.com/ny/api/res/1.2/NtEgwKL6Ro7gdQV.3oKKpg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQzNA--/https://media.zenfs.com/en-us/homerun/tvline.com/035d051c00ecc1104cc20e82d4517fa6`,
-  },
-  {
-    id: 3,
-    text: 'Mr. Robot. "Eps1.1_ones-and-zer0es.mpeg"',
-    url: `https://www.billboard.com/wp-content/uploads/media/Joey-Badass-in-Mr-Robot-press-photo-2017-billboard-1548.jpg?w=1024`,
-  },
-  {
-    id: 4,
-    text: 'balv',
-    url: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR601_nbvoLXfF7jMnQ-N3BYhIC4ZtSiSoqXA&usqp=CAU`,
-  },
-  {
-    id: 5,
-    text: "11 'Mr. Robot' Episodes To Rewatch After That Series Finale Reveal",
-    url: `https://imgix.bustle.com/uploads/image/2019/12/23/c33f7c88-073a-4d7c-bada-d272a7c8759b-screen-shot-2019-12-22-at-23740-pm-1.png?w=1200&h=630&fit=crop&crop=faces&fm=jpg`,
-  },
-];
-
 const words: Word[] = [
   {
     id: 'dd2dfbd3',
@@ -302,6 +229,85 @@ const words: Word[] = [
     fields: ['Сайхан', 'Классный'],
     image:
       'https://cdn.kanobu.ru/editor/images/98/a2903b33-88ee-449d-9cb1-665dc42e1e54.webp',
+  },
+];
+
+export const allTopics: Topic[] = [
+  {
+    id: `1`,
+    url: 'https://teletype.in/files/d9/6a/d96aa6ca-5cc0-49bd-b66b-4f58f9f9a564.jpeg',
+    description: '',
+    title: 'Жилой дом. Квартира.',
+    date: '12.12.12',
+    words,
+  },
+  {
+    id: `2`,
+    url: 'https://begeton.com/files/users-companies/122/3/08/nYB5El3FbbEo7CB67cW3M8qWSjltQEuI.jpg.jpeg',
+    description: '',
+    title: 'Продукты питания',
+    date: 'string',
+    words,
+  },
+  {
+    id: `3`,
+    url: 'https://cdn.kanobu.ru/editor/images/14/ad828eca-6516-4c8d-bffc-7ea8e19caffa.webp',
+    description: '',
+    title: 'Эмоции',
+    date: 'string',
+    words,
+  },
+  {
+    id: `4`,
+    url: 'https://primamediamts.servicecdn.ru/f/big/911/910099.jpg',
+    description: '',
+    title: 'В банке',
+    date: 'string',
+    words,
+  },
+  {
+    id: `5`,
+    url: 'https://assets3.cbsnewsstatic.com/hub/i/r/2017/04/06/0f3e9754-3fd8-481a-bf80-97c1c38c0cf1/thumbnail/1200x630/c8bc2b7238dd83306cefecd875f56a3a/bodegas-20161144.jpg?v=a6a127b4a243923dd1e2140b75f2a43c',
+    description: '',
+    title: 'В магазине',
+    date: 'string',
+    words,
+  },
+  {
+    id: `6`,
+    url: 'https://static.life.ru/publications/2020/5/25/1143568001582.5872.jpg',
+    description: '',
+    title: 'В аэропрту',
+    date: 'string',
+    words,
+  },
+];
+
+export const recentlyPodcasts = [
+  {
+    id: 1,
+    text: 'Mr. Robot by Sam Esmail for USA Network',
+    url: `https://images-na.ssl-images-amazon.com/images/S/pv-target-images/108517acee920b44a8679bf2dd0d9ec29f6728257513e62fd6b5610529185caf._RI_TTW_.jpg`,
+  },
+  {
+    id: 2,
+    text: 'Mr. Robot. "Eps1.1_ones-and-zer0es.mpeg"',
+    url: `https://s.yimg.com/ny/api/res/1.2/NtEgwKL6Ro7gdQV.3oKKpg--/YXBwaWQ9aGlnaGxhbmRlcjt3PTY0MDtoPTQzNA--/https://media.zenfs.com/en-us/homerun/tvline.com/035d051c00ecc1104cc20e82d4517fa6`,
+  },
+  {
+    id: 3,
+    text: 'Mr. Robot. "Eps1.1_ones-and-zer0es.mpeg"',
+    url: `https://www.billboard.com/wp-content/uploads/media/Joey-Badass-in-Mr-Robot-press-photo-2017-billboard-1548.jpg?w=1024`,
+  },
+  {
+    id: 4,
+    text: 'balv',
+    url: `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR601_nbvoLXfF7jMnQ-N3BYhIC4ZtSiSoqXA&usqp=CAU`,
+  },
+  {
+    id: 5,
+    text: "11 'Mr. Robot' Episodes To Rewatch After That Series Finale Reveal",
+    url: `https://imgix.bustle.com/uploads/image/2019/12/23/c33f7c88-073a-4d7c-bada-d272a7c8759b-screen-shot-2019-12-22-at-23740-pm-1.png?w=1200&h=630&fit=crop&crop=faces&fm=jpg`,
   },
 ];
 
