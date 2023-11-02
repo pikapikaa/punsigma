e5c579fdba26e252f560725b991973cdfcf39b30
admin <gorod3012@gmail.com>
2023-11-02 23:15:20 +0800

    *fix

--- src/ui/screens/Main/MainScreen.tsx
+++ src/ui/screens/Main/MainScreen.tsx
@@ -1,4 +1,4 @@
-import React, {useCallback, useRef, useState} from 'react';
+import React, {useCallback} from 'react';
 import {View, StyleSheet} from 'react-native';
 import {ScrollView} from 'react-native-gesture-handler';
 
